009ebc71372873c0e09c83be4eb42063c034cb32
woopii <lozenkrantz@gmail.com>
2024-03-19 01:59:57 +0900

    [ADD] module test

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,6 +2,9 @@ package greetings
 
 import "fmt"
 
-func Hello(name string) {
-	fmt.Println("111")
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
